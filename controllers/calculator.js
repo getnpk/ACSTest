b65cf35388931eb1f932bd9f4e670f1337769d1a
Nitin <getnpk@gmail.com>
2014-03-02 14:06:54 +0800

    working operator apis with full origin access control

---
+++ controllers/calculator.js
@@ -0,0 +1,75 @@
+function add(req, res) {
+    var one = parseFloat(req.query.one);
+    var two = parseFloat(req.query.two);
+    
+    var result;
+    
+    if (isNaN(one) || isNaN(two))
+          result = 'Not a number!';
+    else
+        result = one + two;
+
+    
+    console.log("Result: " + result);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(200, {value : result});
+    
+}
+
+
+function sub(req, res) {
+    var one = parseFloat(req.query.one);
+    var two = parseFloat(req.query.two);
+    
+    var result;
+    
+    if (isNaN(one) || isNaN(two))
+          result = 'Not a number!';
+    else
+        result = one - two;
+    
+    console.log("Result: " + result);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(200, {value : result});
+    
+}
+
+
+function mul(req, res) {
+    
+    var one = parseFloat(req.query.one);
+    var two = parseFloat(req.query.two);
+    
+    var result;
+    
+    if (isNaN(one) || isNaN(two))
+          result = 'Not a number!';
+    else
+        result = one * two;
+    
+    console.log("Result: " + result);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(200, {value : result});
+    
+}
+
+
+function div(req, res) {
+    
+    var one = parseFloat(req.query.one);
+    var two = parseFloat(req.query.two);
+    
+    var result;
+    
+    if (isNaN(one) || isNaN(two))
+          result = 'Not a number!';
+    else if (two === 0)
+        result = 'Cannot divide by 0!';
+    else
+        result = one / two;
+    
+    console.log("Result: " + result);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(200, {value : result});
+    
+}
